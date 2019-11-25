e78e24f413bc280eaf42f78ec81d8f27dd915fce
joshweir <joshua.weir@gmail.com>
2019-11-25 13:30:46 +1100

    add example of mapping over a list in terraform 0.11

--- map-over-a-list.tf
+++ map-over-a-list.tf
@@ -1,14 +1,15 @@
 locals {
+  account_num = "12345"
   my_list = [
-    "arn:aws:sqs:ap-southeast-2:12345:communicate-COM-DATASTAGING-previews-dlq",
-    "arn:aws:sqs:ap-southeast-2:12345:communicate-COM-DATASTAGING-commands-dlq"
+    "arn:aws:sqs:ap-southeast-2:${local.account_num}:communicate-COM-DATASTAGING-previews-dlq",
+    "arn:aws:sqs:ap-southeast-2:${local.account_num}:communicate-COM-DATASTAGING-commands-dlq"
   ]
 }
 
 data "null_data_source" "my_list_base_names_ds" {
   count = "${length(local.my_list)}"
   inputs = {
-    value = "${replace(local.my_list[count.index], "arn:aws:sqs:ap-southeast-2:12345:communicate-COM-DATASTAGING-", "")}"
+    value = "${replace(local.my_list[count.index], "arn:aws:sqs:ap-southeast-2:${local.account_num}:communicate-COM-DATASTAGING-", "")}"
   }
 }
 
